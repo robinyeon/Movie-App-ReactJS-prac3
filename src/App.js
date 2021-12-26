912ec873182b595a96b7f9a7c09e90e70b6cf75a
robinyeon <robin.yeon@gmail.com>
2021-12-26 19:04:36 +0900

    updated: retrieved movies' info

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  // useEffect(() => {
+  //   fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`)
+  //     .then((res) => res.json())
+  //     .then((json) => {
+  //       setMovies(json.data.movies)
+  //       setLoading(false)
+  //     })
+  // }, [])
+
+  // async: "얜 비동기 할거다? 어떤 비동기냐면, 이 안에선 await를 사용해서 순서대로 내려갈거야."
+  const getMovies = async () => {
+    const response = await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+    );
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  }
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  console.log(movies);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading
+        ? <strong>LOADING</strong>
+        : <div>{movies.map((movie) => (
+          <div key={movie.id}>
+            <img src={movie.medium_cover_image} alt="" />
+            <h2>{movie.title}</h2>
+            <p>{movie.summary}</p>
+            <ul>
+              {movie.genres.map((genre, index) => <li key={index}>{genre}</li>)}
+            </ul>
+          </div>))}</div>}
     </div>
   );
 }
